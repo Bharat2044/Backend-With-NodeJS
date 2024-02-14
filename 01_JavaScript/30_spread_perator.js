782edf248e32716dcdfb5d029a260c8e3c1fef23
Bharat2044 <bk9955204456@gmail.com>
2024-02-14 16:23:53 +0530

    code added

---
+++ 01_JavaScript/30_spread_perator.js
@@ -0,0 +1,23 @@
+/**
+ * For Arrays
+ */
+
+let arr1 = [1, 2, 3];
+let arr2 = [...arr1];
+let arr3 = [...arr1, 4, 5];
+
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
+
+
+/**
+ * For Objects
+ */
+let obj1 = {a: 1, b: 2};
+let obj2 = {...obj1};
+let obj3 = {...obj1, c: 3};
+
+console.log(obj1);
+console.log(obj2);
+console.log(obj3);
