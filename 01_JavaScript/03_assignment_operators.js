5727760302cab33bdac8b9d4ac8fff3c39e44eef
Bharat2044 <bk9955204456@gmail.com>
2024-02-14 16:21:25 +0530

    code added

---
+++ 01_JavaScript/03_assignment_operators.js
@@ -0,0 +1,15 @@
+let name = "Bharat";
+console.log(name);
+
+let x = 10;
+console.log(x);
+x += 10;
+console.log(x);
+
+let y = 10;
+y -= 5;
+console.log(y);
+
+let z = 10;
+z *= 2;
+console.log(z);
