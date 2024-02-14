4fa6ddd2ad24367d65a44d195d2852545a8650ed
Bharat2044 <bk9955204456@gmail.com>
2024-02-14 16:21:50 +0530

    code added

---
+++ 01_JavaScript/05_increment_decrement_operators.js
@@ -0,0 +1,16 @@
+let a = 5;
+let b = ++a;
+console.log(a, b);
+
+let c = 5;
+let d = c++;
+console.log(c, d);
+
+
+let e = 5;
+let f = --e;
+console.log(e, f);
+
+let g = 5;
+let h = g--;
+console.log(g, h);
