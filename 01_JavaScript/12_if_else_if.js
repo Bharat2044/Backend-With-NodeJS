0c5dfb1bc11efad6a15a211bd423c5ba3cb334f5
Bharat2044 <bk9955204456@gmail.com>
2024-02-14 16:22:23 +0530

    code added

---
+++ 01_JavaScript/12_if_else_if.js
@@ -0,0 +1,17 @@
+let day = 'Friday';
+
+if(day == "Monday") {
+    console.log("Monday");
+} else if(day == "Tuesday") {
+    console.log("Tuesday");
+} else if(day == "Wednesday") {
+    console.log("Wednesday");
+} else if(day == "Thursday") {
+    console.log("Thursday");
+} else if(day == "Friday") {
+    console.log("Friday");
+} else if(day == "Saturday") {
+    console.log("Saturday");
+} else {
+    console.log("Sunday");
+}
