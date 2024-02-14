64c905ff7bd6a65698a4093877d66dca22f537a3
Bharat2044 <bk9955204456@gmail.com>
2024-02-14 16:23:14 +0530

    code added

---
+++ 01_JavaScript/21_iffe.js
@@ -0,0 +1,12 @@
+/**
+  * IIFE -> Immediately Invoked Function Expression
+  */
+(function(){
+    console.log('Hello Everyone!');
+})();
+
+
+(function(a, b){
+    console.log(a + b);
+ })(2, 3);
+
