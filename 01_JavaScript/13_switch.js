cba64a52967d3c5a37935ed9027a99ff36564979
Bharat2044 <bk9955204456@gmail.com>
2024-02-14 16:22:31 +0530

    code added

---
+++ 01_JavaScript/13_switch.js
@@ -0,0 +1,23 @@
+let fruit = 'Mango';
+
+switch (fruit) {
+    case 'Apple':
+        console.log('Apple');
+        break;
+
+    case 'Mango':
+        console.log('Mango');
+        break;
+
+    case 'Banana':
+        console.log('Banana');
+        break;
+
+    case 'Orange':
+        console.log('Orange');
+        break;
+
+    default:
+        console.log('Not a valid fruit');
+        break;
+}
