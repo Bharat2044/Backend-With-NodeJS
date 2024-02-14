0054c0dee344e92661dd0d6404fa3c798278c6d9
Bharat2044 <bk9955204456@gmail.com>
2024-02-14 16:33:00 +0530

    code added

---
+++ 02_NodeJS/03_module/01_user-defined-module_or_custom-module/myModule.js
@@ -0,0 +1,18 @@
+// let add = (a, b) => a + b;
+// module.exports = add;
+
+
+/*
+let add = (a, b) => a + b;
+let prod = (a, b) => a * b;
+*/
+/*
+module.exports = {
+    add: add,
+    prod: prod
+}
+*/
+
+
+exports.add = (a, b) => a + b;
+exports.prod = (a, b) => a * b;
