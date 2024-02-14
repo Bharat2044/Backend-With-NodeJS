cf7447729c4e14cb92764ebe42475482f7397c59
Bharat2044 <bk9955204456@gmail.com>
2024-02-14 16:21:53 +0530

    code added

---
+++ 01_JavaScript/06_relational_operators.js
@@ -0,0 +1,31 @@
+console.log(1 == 1);
+console.log(1 == "1");
+console.log(1 != 1);
+console.log(1 != "1");
+
+console.log();
+
+console.log(1 === 1);
+console.log(1 === "1");
+console.log(1 !== 1);
+console.log(1 !== "1");
+
+console.log();
+
+console.log(1 > 1);
+console.log(2 > 1);
+
+console.log();
+
+console.log(1 >= 1);
+console.log(2 >= 1);
+
+console.log();
+
+console.log(1 < 1);
+console.log(2 < 1);
+
+console.log();
+
+console.log(1 <= 1);
+console.log(2 <= 1);
