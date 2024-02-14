01b44b0d7684cb468437e496a4d19222093caf8f
Bharat2044 <bk9955204456@gmail.com>
2024-02-14 16:22:11 +0530

    code added

---
+++ 01_JavaScript/09_ternary_operator.js
@@ -0,0 +1,5 @@
+(true) ? console.log("true part") : console.log("false pary");
+(false) ? console.log("true part") : console.log("false pary");
+
+
+(2 > 3) ? console.log("2 is greater") : console.log("3 is greater");;
