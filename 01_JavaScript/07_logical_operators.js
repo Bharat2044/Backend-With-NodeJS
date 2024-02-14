98aa70f358ebdbe030d487c4969504c98e3f29cf
Bharat2044 <bk9955204456@gmail.com>
2024-02-14 16:21:57 +0530

    code added

---
+++ 01_JavaScript/07_logical_operators.js
@@ -0,0 +1,23 @@
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+console.log();
+
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+console.log();
+
+console.log(!true);
+console.log(!false);
+
+console.log();
+
+console.log(3 > 4 && true);
+console.log(3 > 4 || true);
+console.log(2 < 3 && 5 > 1);
+
