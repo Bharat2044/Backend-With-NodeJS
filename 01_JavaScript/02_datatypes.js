c012266689afdfa1d913975c2a5d6efe9e82f64d
Bharat2044 <bk9955204456@gmail.com>
2024-02-14 16:21:22 +0530

    code added

---
+++ 01_JavaScript/02_datatypes.js
@@ -0,0 +1,45 @@
+// string
+let name = "Bharat";
+console.log(typeof name, name);
+
+// number
+let num1 = 100;
+let num2 = 5.9;
+console.log(typeof num1, num1);
+console.log(typeof num2, num2);
+
+// BigInt
+let num = 1001n;
+console.log(typeof num, num);
+
+// NaN
+let x = NaN;
+console.log(typeof x, x);
+
+// null
+let y = null;
+console.log(typeof y, y);
+
+// undefined
+let z = undefined;
+console.log(typeof z, z);
+
+// undefined
+let isMarried = false;
+console.log(typeof isMarried, isMarried);
+
+// Symbol
+let s = Symbol();
+console.log(typeof s, s);
+
+
+// object
+let arr = [1, 'Bharat', true, 5.6];
+console.log(typeof arr, arr);
+
+let person = {
+    name: "Bharat",
+    age: 21, 
+    isStudent: true
+}
+console.log(typeof person, person);
