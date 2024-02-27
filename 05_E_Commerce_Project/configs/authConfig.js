38c83e85c5396b1bae0b40fcb4ac5ced2f19a85d
Bharat2044 <bk9955204456@gmail.com>
2024-02-27 22:08:22 +0530

    code added

---
+++ 05_E_Commerce_Project/configs/authConfig.js
@@ -0,0 +1,8 @@
+// This module.exports statement is used to export an object containing properties.
+module.exports = {
+
+    // 'secret' is a key in the exported object, and its value is a string.
+    // This string represents a secret key or passphrase. It is important to keep
+    // such sensitive information secure and not expose it in your code repository.
+    secret: "This is my super private secret"
+}
