299e6f2e83b38b2cd265fa393a08688b65652712
Bharat2044 <bk9955204456@gmail.com>
2024-02-27 22:08:16 +0530

    code added

---
+++ 05_E_Commerce_Project/configs/databaseConfig.js
@@ -0,0 +1,15 @@
+/**
+ * databaseConfig.js
+ * 
+ * This file contains the configuration settings for the MongoDB database.
+ */
+
+
+// Module exporting the database configuration
+module.exports = {
+    // Name of the MongoDB database
+    DB_NAME: "ecom_db",
+
+    // URL for connecting to the MongoDB database
+    DB_URL: "mongodb://127.0.0.1/ecomm_db"
+};
