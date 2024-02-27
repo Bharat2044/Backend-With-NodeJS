45d995b9aa30af2fe9e3c6340d2e4dec041317ab
Bharat2044 <bk9955204456@gmail.com>
2024-02-27 22:08:31 +0530

    code added

---
+++ 05_E_Commerce_Project/configs/serverConfig.js
@@ -0,0 +1,11 @@
+/**
+ * serverConfig.js
+ * 
+ * This file contains configuration settings for the server.
+ */
+
+// Module exporting server configuration
+module.exports = {
+    // Port number for the server to listen on
+    PORT: 8888, // Change this value to set a different port
+};
