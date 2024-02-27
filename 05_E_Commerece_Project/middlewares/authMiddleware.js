b15c46c3600cf76397c45e32df97d436de87b252
Bharat2044 <bk9955204456@gmail.com>
2024-02-27 17:30:37 +0530

    Signup code

---
+++ 05_E_Commerece_Project/middlewares/authMiddleware.js
@@ -0,0 +1,53 @@
+const userModel = require("../models/userModel");
+
+/**
+ * Middleware function to check if the request body is proper and correct for user sign-up.
+ */
+
+const verifySignUpBody = async (req, res, next) => {
+
+    try {
+
+        // Check for the name
+        if (!req.body.name) {
+            return res.status(400).send({
+                message: "Failed! Name was not provided in the request body"
+            });
+        }
+
+        // Check for the email
+        if (!req.body.email) {
+            return res.status(400).send({
+                message: "Failed! Email was not provided in the request body"
+            });
+        }
+
+        // Check for the userId
+        if (!req.body.userId) {
+            return res.status(400).send({
+                message: "Failed! userId was not provided in the request body"
+            });
+        }
+
+        // Check if the user with the same userId is already present
+        const user = await userModel.findOne({ userId: req.body.userId });
+
+        if (user) {
+            return res.status(400).send({
+                message: "Failed! User with the same userId is already present"
+            });
+        }
+
+        next();
+
+    } catch (err) {
+        console.log("Error while validating the request object", err);
+        res.status(500).send({
+            message: "Error while validating the request body"
+        });
+    }
+};
+
+module.exports = {
+    verifySignUpBody: verifySignUpBody
+};
