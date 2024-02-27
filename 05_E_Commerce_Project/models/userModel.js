2e9702770322d5058d8a35d9fea70a665d7117bd
Bharat2044 <bk9955204456@gmail.com>
2024-02-27 22:08:44 +0530

    code added

---
+++ 05_E_Commerce_Project/models/userModel.js
@@ -0,0 +1,50 @@
+// Importing Mongoose library for MongoDB schema modeling
+const mongoose = require("mongoose");
+
+/**
+ * User Schema:
+ * - Defines the structure of the User model in the database
+ * 
+ * Fields:
+ *   - name: User's name
+ *   - userId: Unique identifier for the user
+ *   - password: User's password
+ *   - email: User's email address (unique and in lowercase)
+ *   - userType: Type of user (default: "CUSTOMER", enum: ["CUSTOMER", "ADMIN"])
+ */
+
+// Creating the User schema using Mongoose
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      minLength: 10,
+      unique: true
+    },
+    userType: {
+        type: String,
+        required: true,
+        default: "CUSTOMER",
+        enum: ["CUSTOMER", "ADMIN"]
+    }
+  },
+  { versionKey: false, timestamps: true } // Configuration options: versionKey disabled, timestamps for createdAt and updatedAt
+);
+
+// Exporting the User model based on the defined schema
+module.exports = mongoose.model("User", userSchema);
