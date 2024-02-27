ef88775ad8f408d5e1d0801e37f254009f2583a5
Bharat2044 <bk9955204456@gmail.com>
2024-02-27 20:21:07 +0530

    login API

--- 05_E_Commerece_Project/routes/authRoute.js
+++ 05_E_Commerece_Project/routes/authRoute.js
@@ -1,16 +1,40 @@
 /**
- * Route Configuration for User Signup
+ * Route Configuration for User Signup and Signin
  * 
- * This file sets up the route for handling user signup requests.
- * The route is defined as a POST request to "/ecomm/api/v1/auth/signup".
- * It includes middleware for verifying the signup request body and calls the signup function in the authController.
+ * This file sets up the route for handling user signup and signin requests.
+ * It includes middleware for verifying the signup request body and calls the corresponding functions in the authController.
  */
 
+// Import the necessary controllers and middlewares
 const authController = require("../controllers/authController");
 const authMiddleware = require("../middlewares/authMiddleware");
 
 // Exporting a function that sets up the route on the provided Express app
 module.exports = (app) => {
+
     // Define a POST route for user signup
+    /**
+     * Define a POST route for user signup
+     * POST localhost:8888/ecomm/api/v1/auth/signup
+     * 
+     * Middleware:
+     * - authMiddleware.verifySignUpBody: Verifies the request body for signup to ensure it meets the required criteria.
+     * 
+     * Controller:
+     * - authController.signup: Handles the signup request and initiates the user creation process.
+     */
     app.post("/ecomm/api/v1/auth/signup", [authMiddleware.verifySignUpBody], authController.signup);
+
+    // Define a POST route for user signin
+    /**
+     * Define a POST route for user signin
+     * POST localhost:8888/ecomm/api/v1/auth/signin
+     * 
+     * Middleware:
+     * - authMiddleware.verifySignUpBody: Verifies the request body for signin to ensure it meets the required criteria.
+     * 
+     * Controller:
+     * - authController.signin: Handles the signin request and initiates the user authentication process.
+     */
+    app.post("/ecomm/api/v1/auth/signin", [authMiddleware.verifySignUpBody], authController.signin);
 };
