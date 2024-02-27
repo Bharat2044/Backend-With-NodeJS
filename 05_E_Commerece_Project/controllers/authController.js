c640a4a493b0f87060ac19872edcff31be06d7da
Bharat2044 <bk9955204456@gmail.com>
2024-02-27 20:21:03 +0530

    login API

--- 05_E_Commerece_Project/controllers/authController.js
+++ 05_E_Commerece_Project/controllers/authController.js
@@ -5,7 +5,10 @@
  */
 
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const userModel = require("../models/userModel");
+const authConfig = require("../configs/authConfig")
 
 // Controller function for user registration
 exports.signup = async (req, res) => {
@@ -49,10 +52,46 @@ exports.signup = async (req, res) => {
     } catch (err) {
         // Handle errors and send appropriate error response
         console.log("Error while registering the user", err);
+        
         res.status(500).send({
             message: "Some error happened while registering the user"
         });
     }
 
     // 3. Return the response back to the user
+    
 };
+
+
+
+exports.signin = async (req, res) => {
+    // 1. Check if the user id is present in the system
+    const user = await userModel.findOne({userId: req.body.userId});
+    
+    if(user == null) {
+        return res.status(400).send({
+            message: "User id is not a valid user id"
+        });
+    }
+
+    // 2. Password is correct, then login allowed
+    const isPasswordValid  = bcrypt.compareSync(req.body.password, user.password);
+    
+    if(!isPasswordValid) {
+        return res.status(401).send({
+            message: "Wrong password passed"
+        });
+    }
+
+    // 3. Using JWT we will create the access token with a given TTL(Time To Live) and return
+    const token = jwt.sign({id: user.userId}, authConfig.secret, {expiresIn: 120});
+
+    res.status(200).send({
+        name: user.name,
+        userId: user.userId,
+        email: user.email,
+        userType: user.userType,
+        accessToken: token
+    });
+
+}
